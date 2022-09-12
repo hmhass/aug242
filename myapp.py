287dad50a712f69006e929a12ea5d5053620bbca
hmhass <hmhassell@gmail.com>
2022-09-11 23:22:18 -0400

    Dope

--- myapp.py
+++ myapp.py
@@ -4,7 +4,19 @@ app = Flask(__name__)
 
 @app.route("/banana/meep", methods=["GET"])
 def mymethod ():
-    return "Ok"
+    token2 = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhSRFQiLCJzdWIiOiJCNEYzNVEiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcm94eSBycHJvIHJudXQgcnNsZSByYWN0IHJsb2MgcnJlcyByd2VpIHJociBydGVtIiwiZXhwIjoxNjkzNDg4MDQxLCJpYXQiOjE2NjE5NTIwNDF9.uk4UyLwyQeLjnoE6jxKPNCxfkzs0mFTq_09cfuyV74U"
+    header = {'Accept' : 'application/json', 'Authorization' : 'Bearer {}'.format(token2)}
+    hearturl = "https://api.fitbit.com/1/user/-/activities/heart/date/today/1d/1min.json"
+    resp = requests.get(hearturl, headers=header).json()
+    heart = resp['activities-heart-intraday']['dataset'][-1]
+    hearttime = heart["time"]
+    now = datetime.datetime.now()
+    newht = datetime.datetime.strptime(hearttime, "%H:%M:%S")
+    diffy = now - newht
+    diffmins = math.floor(diffy.seconds / 60)
+    heartrate = heart["value"]
+    ret = {'heart-rate':heartrate, 'time offset':diffmins}
+    return ret
 
 @app.route("/poopy", methods=["GET"])
 def mymethod2 ():
