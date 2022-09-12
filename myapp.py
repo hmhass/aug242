f580fef78c62a8bb01dac2d805cbf5e895738573
hmhass <hmhassell@gmail.com>
2022-09-11 21:52:03 -0400

    more

--- myapp.py
+++ myapp.py
@@ -4,5 +4,8 @@ app = Flask(__name__)
 @app.route("/banana/meep", methods=["GET"])
 def mymethod ():
     return "Why did the chicken cross the street? Ha! ha, ha!"
+@app.route("/poopy", methods=["GET"])
+def mymethod ():
+    return "Test"
 if __name__ == '__main__':
     app.run(debug=True)
